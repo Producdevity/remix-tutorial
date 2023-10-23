0328a08c0aa3885a7e7f77c9e9b25a526e0637a9
Yassine Gherbi <yassine@hexelnet.com>
2023-10-23 05:40:30 +0200

    feat: edit contact ui

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,12 @@ import {
 import type { LinksFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import appStylesHref from "./app.css";
-import { getContacts } from "./data";
+import { createEmptyContact, getContacts } from "./data";
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
+};
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
